bebb12521c0cdc88f080dc45ca0ec6e8fd2fc65f
mitsu1119 <hachimitsu1119@gmail.com>
2020-01-12 12:14:49 +0900

    My MDI template. burn

--- UnitMaker/UnitMaker.h
+++ UnitMaker/UnitMaker.h
@@ -2,43 +2,99 @@
 #include "resource.h"
 #include <Windows.h>
 #include <tchar.h>
-#include <stdlib.h>
-#include <string.h>
+#include <string>
 
-#define ID_CHILDWND 50001
-#define TITLE _T("UnitMaker")
-#define IDTEXT_MAINWND _T("UnitMaker")
-#define IDTEXT_MDICLIENTWND _T("MDIClient")
-#define IDTEXT_CHILD _T("MDIChild")
+#define IDM_FIRSTCHILD 0x100
+#define MYMENU MAKEINTRESOURCE(IDR_MENU1)
+#define MDICLIENT _T("MDICLIENT")
+#define EDIT _T("EDIT")
 
-HINSTANCE hInst;
+extern TCHAR szFrameClassName[];
+extern TCHAR szChildDoc[];
+extern HINSTANCE hInst;
+extern HMENU hMenuFirst, hMenuFirstWnd;
+extern HMENU hMenuFirst, hMenuFirstWnd;
 
-// Processing messages.
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-	HDC hdc;
-	CLIENTCREATESTRUCT ccsClient;
-	static HWND hClientWindow;
+// Main frame.
+LRESULT CALLBACK FrameWndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+	static HWND hClient;
+	HWND hChild;
+	CLIENTCREATESTRUCT ccs;
+	MDICREATESTRUCT mdic;
 
-	switch(message) {
+	switch(msg) {
+	case WM_CREATE:
+		ccs.hWindowMenu = hMenuFirstWnd;
+		ccs.idFirstChild = IDM_FIRSTCHILD;
+		hClient = CreateWindow(MDICLIENT, NULL, WS_CHILD | WS_VISIBLE | WS_CLIPCHILDREN, 0, 0, 0, 0, hWnd, (HMENU)1, hInst, &ccs);
+		return 0;
+	case WM_COMMAND:
+		switch(LOWORD(wp)) {
+		case IDM_FILE_NEW:
+			mdic.szClass = szChildDoc;
+			mdic.szTitle = _T("Document");
+			mdic.hOwner = hInst;
+			mdic.x = CW_USEDEFAULT;
+			mdic.y = CW_USEDEFAULT;
+			mdic.cx = CW_USEDEFAULT;
+			mdic.cy = CW_USEDEFAULT;
+			mdic.style = 0;
+			mdic.lParam = 0;
+			hChild = (HWND)SendMessage(hClient, WM_MDICREATE, 0, (LPARAM)&mdic);
+			return 0;
+		default:
+			hChild = (HWND)SendMessage(hClient, WM_MDIGETACTIVE, 0, 0);
+			if(IsWindow(hChild)) SendMessage(hChild, WM_COMMAND, wp, lp);
+			break;
+		}
+	case WM_QUERYENDSESSION:
+	case WM_CLOSE:
+		if(GetWindow(hClient, GW_CHILD)) return 0;
+		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
-	case WM_CREATE:
-		ccsClient.hWindowMenu = NULL;
-		ccsClient.idFirstChild = ID_CHILDWND;
-		hClientWindow = CreateWindow(IDTEXT_MDICLIENTWND, NULL, WS_CHILD | WS_CLIPCHILDREN | WS_VISIBLE, 0, 0, 0, 0, hWnd, (HMENU)1, hInst, &ccsClient);
-		break;
-		return 0;
-	case WM_COMMAND:
-		switch(LOWORD(wParam)) {
-		case ID_FILE_NEW:
-			CreateMDIWindow(IDTEXT_CHILD, _T("New"), 0, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, hClientWindow, hInst, 0);
-			break;
-		case ID_FILE_OPEN:
-			CreateMDIWindow(IDTEXT_CHILD, _T("Open"), 0, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, hClientWindow, hInst, 0);
-			break;
-		}
-		return 0;
 	}
-	return DefFrameProc(hWnd, hClientWindow, message, wParam, lParam);
+	return DefFrameProc(hWnd, hClient, msg, wp, lp);
+}
+
+// Child document window.
+LRESULT CALLBACK DocProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+	static HWND hClient, hFrame;
+	HWND hEdit;
+	RECT rc;
+
+	switch(msg) {
+	case WM_CREATE:
+		hClient = GetParent(hWnd);
+		hFrame = GetParent(hClient);
+		GetClientRect(hWnd, &rc);
+		hEdit = CreateWindow(EDIT, NULL, WS_VISIBLE | WS_CHILD | ES_WANTRETURN | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL | ES_AUTOHSCROLL | WS_HSCROLL, rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top, hWnd, NULL, hInst, NULL);
+		return 0;
+	case WM_SIZE:
+		GetClientRect(hWnd, &rc);
+		hEdit = GetWindow(hWnd, GW_CHILD);
+		MoveWindow(hEdit, rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top, TRUE);
+		break;
+	}
+	return DefMDIChildProc(hWnd, msg, wp, lp);
+}
+
+// Window class regist.
+int MyRegisterWC(WNDPROC WndProc, LPCTSTR ClassName, HBRUSH BackGround) {
+	WNDCLASSEX wc;
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInst;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = BackGround;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = ClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	return (RegisterClassEx(&wc));
 }
--- UnitMaker/main.cpp
+++ UnitMaker/main.cpp
@@ -1,44 +1,39 @@
 #include "UnitMaker.h"
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE hInst;
+
+// Frame window class.
+TCHAR szFrameClassName[] = _T("framewindow");
+
+// Child window class.
+TCHAR szChildDoc[] = _T("document");
+
+// Menu
+HMENU hMenuFirst, hMenuFirstWnd;
 
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
-	WNDCLASS wcex;
-	// wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_APPWORKSPACE + 1);
-	wcex.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
-	wcex.lpszClassName = IDTEXT_MAINWND;
-	// wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
-
-	if(!RegisterClass(&wcex)) return 1;
-
-	wcex.lpfnWndProc = DefMDIChildProc;
-	wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wcex.lpszClassName = IDTEXT_CHILD;
-
-	if(!RegisterClass(&wcex)) return 1;
-
-	// Store instance handle in global variable.
 	hInst = hInstance;
 
-	HWND hWnd = CreateWindow(IDTEXT_MAINWND, TITLE, WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
-	if(!hWnd) return 1;
+	if(!MyRegisterWC(FrameWndProc, szFrameClassName, (HBRUSH)(COLOR_APPWORKSPACE+1))) return 1;
+	if(!MyRegisterWC(DocProc, szChildDoc, (HBRUSH)GetStockObject(WHITE_BRUSH))) return 1;
 
-	ShowWindow(hWnd, nCmdShow);
-	UpdateWindow(hWnd);
+	// Setting menu bar in frame window.
+	hMenuFirst = LoadMenu(hInst, MYMENU);
+	hMenuFirstWnd = GetSubMenu(hMenuFirst, 0);
 
+	HWND hFrame = CreateWindow(szFrameClassName, _T("UnitMaker"), WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, hMenuFirst, hInst, NULL);
+	HWND hClient = GetWindow(hFrame, GW_CHILD);
+
+	ShowWindow(hFrame, nCmdShow);
+	UpdateWindow(hFrame);
 	MSG msg;
 	while(GetMessage(&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		if(!TranslateMDISysAccel(hClient, &msg)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 	}
 
+	DestroyMenu(hMenuFirst);
 	return (int)msg.wParam;
 }
